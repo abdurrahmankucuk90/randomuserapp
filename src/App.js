b9fa764c2be99e8b44f974bb4f7025945f273c3e
abdurrahmankucuk90 <abdurrahmankucuk90@gmail.com>
2022-07-02 16:35:31 +0200

    asdf

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 
 import './App.css';
+import Footer from './components/Footer/Footer';
 import Navbar from './components/Navbar/Navbar';
+import PersonInfo from './components/PersonInfo/PersonInfo';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <PersonInfo/>
+      <Footer/>
     </div>
   );
 }
